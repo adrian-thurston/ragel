e86dcfe363ae27827531d0d31c134a5c6d68ff06
Adrian Thurston <thurston@complang.org>
2004-05-16 21:03:46 +0000

    updated overall description

--- doc/mainpage.h
+++ doc/mainpage.h
@@ -3,14 +3,28 @@
  *
  * \section Introduction
  *
- * Aapl is a C++ template library. It provides much of the functionality found
- * in the STL, but aims to more versatile than typical template libraries.
- * There are many different 'flavours' of the data structures allowing the
- * programmer to have it their own way. Aapl data structures do not have
- * private data members.
+ * Aapl is a C++ template library for generic programming. It contains
+ * implementations of %AVL Tree, Linked List, %Vector, Binary Search %Table,
+ * Queue, %String and %Sort.
  *
- * Aapl does not conform to any known standards. It is a custom job designed
- * solely to meet the tastes of the author.
+ * Aapl supports different generic programming paradigms by providing
+ * variations of standard data structures. For example, a by-value linked list
+ * template may be used to store a user supplied type such as an integer. A
+ * different list template allows the user to define the data structure that
+ * is to be used as the element. A third list template allows a single
+ * instance of a data structure to be an element in multiple lists.
+ *
+ * Wherever possible, Aapl data structures do not depend on heap memory
+ * allocation. There are variations of the linked list and AVL tree that allow
+ * the programmer to allocate a collection of elements statically and
+ * insert/remove them at will.
+ *
+ * Aapl data structures do not have data members that are hidden behind a
+ * strict abstraction layer. Aapl makes very little use of the private
+ * keyword.  Though data abstractions can be a useful programming technique to
+ * quickly produce very robust code, they can inhibit functionality when the
+ * data structure is the centre of much attention. Therefore Aapl leaves the
+ * use of abstractions up to the programmer.
  *
  * Browse documentation by:
  * <ul>
