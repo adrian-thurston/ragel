e2c401371a9db146b5b579093748ca3acbf4b2ba
Adrian Thurston <thurston@complang.org>
2007-01-27 18:05:18 +0000

    Finished replacement of manually drawn figures with graphviz drawn figures. These need bounding box fixes at the very least.

--- doc/genfigs.sh
+++ doc/genfigs.sh
@@ -12,8 +12,10 @@ fi
 for fig in $figs; do
 	if awk -f extract.awk -vexname=$fig $input > /dev/null; then
 		echo generating ${fig}.fig
+		opt=`awk -f extract.awk -vexname=$fig $input | 
+				sed '/^ *OPT:/s/^.*: *//p;d'`
 		awk -f extract.awk -vexname=$fig $input | \
-			ragel | rlcodegen -Vp | \
+			ragel | rlcodegen -V $opt | \
 			dot -Tfig | awk -f fixbackbox.awk > ${fig}.fig;
 	else
 		echo "$0: internal error: figure $fig not found in $input" >&2
