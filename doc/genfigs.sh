c6d652a2ed858766551f9c79fb498854383c598e
Adrian Thurston <thurston@complang.org>
2008-01-10 18:52:06 +0000

    Editing of chapter three. Removed the main label and reduced the state size in the two diagrams that need to the updated for 6.0.

--- doc/genfigs.sh
+++ doc/genfigs.sh
@@ -5,12 +5,11 @@ input=ragel-guide.tex
 
 for fig; do
 	if awk -f extract.awk -vexname=$fig $input > /dev/null; then
-		echo generating ${fig}.fig
+		echo generating ${fig}.dot
 		opt=`awk -f extract.awk -vexname=$fig $input | 
 				sed '/^ *OPT:/s/^.*: *//p;d'`
-		awk -f extract.awk -vexname=$fig $input | \
-			../ragel/ragel | ../rlgen-dot/rlgen-dot $opt | \
-			dot -Tfig > ${fig}.fig;
+		awk -f extract.awk -vexname=$fig $input > ${fig}.rl
+		../ragel/ragel -V -p ${fig}.rl > ${fig}.dot
 	else
 		echo "$0: internal error: figure $fig not found in $input" >&2
 		exit 1
