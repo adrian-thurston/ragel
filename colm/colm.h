97281aab6ad5740938674f34073496eb25801d8f
Adrian Thurston <thurston@complang.org>
2011-10-24 16:10:46 +0000

    Added missing colm.h.

---
+++ colm/colm.h
@@ -0,0 +1,13 @@
+#ifndef _COLM_H
+#define _COLM_H
+
+#include <colm/pdarun.h>
+#include <colm/fsmrun.h>
+#include <colm/debug.h>
+#include <colm/bytecode.h>
+#include <colm/config.h>
+#include <colm/defs.h>
+#include <colm/input.h>
+#include <colm/tree.h>
+
+#endif
