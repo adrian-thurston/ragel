bfb4d49fbfd955914fa4a03ab4eae8557a885e85
Adrian Thurston <thurston@complang.org>
2013-11-17 15:35:16 -0500

    support D array syntax

--- src/codegen.cc
+++ src/codegen.cc
@@ -119,7 +119,7 @@ void TableArray::finishAnalyze()
 void TableArray::startGenerate()
 {
 	out << "array " << type << " " << 
-		"_" << codeGen.DATA_PREFIX() << name << "[] = {\n\t";
+		"_" << codeGen.DATA_PREFIX() << name << " = {\n\t";
 }
 
 void TableArray::valueGenerate( long long v )
