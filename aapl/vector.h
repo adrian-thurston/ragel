6fbbd0df6263108543772ce46b7f1dd75c88d1a2
Adrian Thurston <thurston@complang.org>
2015-01-23 12:05:08 -0500

    revert "removed LGPL license from code"
    This reverts commit 5af13e13462609230f319b76981c9199f4f086b1.

--- aapl/vector.h
+++ aapl/vector.h
@@ -2,6 +2,23 @@
  *  Copyright 2002, 2006 Adrian Thurston <thurston@complang.org>
  */
 
+/*  This file is part of Aapl.
+ *
+ *  Aapl is free software; you can redistribute it and/or modify it under the
+ *  terms of the GNU Lesser General Public License as published by the Free
+ *  Software Foundation; either version 2.1 of the License, or (at your option)
+ *  any later version.
+ *
+ *  Aapl is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ *  more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Aapl; if not, write to the Free Software Foundation, Inc., 59
+ *  Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 #ifndef _AAPL_VECTOR_H
 #define _AAPL_VECTOR_H
 
