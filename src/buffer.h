b0fb5da493f4dedcd0e80f1f7737e37f56f254b1
Adrian Thurston <thurston@colm.net>
2016-10-17 09:53:41 -0600

    added Buffer
    Buffer is a simple growable array, which, when reset, does not down-resize.

---
+++ src/buffer.h
@@ -0,0 +1,59 @@
+/*
+ *  Copyright 2003 Adrian Thurston <thurston@complang.org>
+ */
+
+/*  This file is part of Colm.
+ *
+ *  Colm is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  Colm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with Colm; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
+#ifndef _AAPL_BUFFER_H
+#define _AAPL_BUFFER_H
+
+/* Buffer is an automatically grown buffer for collecting tokens. Always reuses
+ * space. Never down resizes. Clear is a cheap operation. */
+struct Buffer
+{
+	static const int BUFFER_INITIAL_SIZE = 4096;
+
+	Buffer()
+	{
+		data = (char*) malloc( BUFFER_INITIAL_SIZE );
+		allocated = BUFFER_INITIAL_SIZE;
+		length = 0;
+	}
+
+	~Buffer()
+	{
+		free(data);
+	}
+
+	void append( char p )
+	{
+		if ( length == allocated ) {
+			allocated *= 2;
+			data = (char*) realloc( data, allocated );
+		}
+		data[length++] = p;
+	}
+		
+	void clear() { length = 0; }
+
+	char *data;
+	int allocated;
+	int length;
+};
+
+#endif
