9b12a3fe5a74dbe5e29ac686a94ccb84bc2a71a4
Adrian Thurston <thurston@colm.net>
2017-07-31 13:49:51 -0400

    started driver for out-of-tree host lang support

---
+++ src/host-ruby/driver.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#
+# Parse args enough to get language, input, output, include args. Stash rest.
+#
+
+lang=""
+input=""
+output=""
+include=""
+
+while getopts "Ro:I:" opt; do
+	case $opt in
+		R)
+			lang=ruby;
+		;;
+		o)
+			output="$OPTARG"
+		;;
+		I)
+			include="$include $OPTARG"
+		;;
+		*)
+			exit 1;
+		;;
+	esac
+done
+
+shift $((OPTIND - 1));
+input=$1
+
+# derive an output file name from input if not present.
+if [ -z "$output" ]; then
+	output=${input%.rl}.c
+	if [ "$input" = "$output" ]; then
+		echo input equal output
+	fi
+fi
+
+# Derive a root for output purposes.
+root=${output%.*}
+
+set -x
+
+./$lang $root.rfi $input $include
+../ragel -P -o $output $root.rfi
+#./-$lang -o $output root.rbi
