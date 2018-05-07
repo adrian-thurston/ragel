335a1358877c7d01334d173731bc6b70a62ed3a7
Adrian Thurston <thurston@colm.net>
2018-05-07 11:41:18 -0400

    take input file from first argument

--- free-bytecodes.sh
+++ free-bytecodes.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
 
+IN=$1
+
 diff -U0 \
 <(
   for i in `seq 0 255`; do
     printf "0x%02x\\n" $i
   done
 ) \
-<(sed -n '/^ *$/d; s/#define [A-Z0-9_]* *//; p' in | sort | uniq)
+<(sed -n '/^ *$/d; s/#define [A-Z0-9_]* *//; p' $IN | sort | uniq)
