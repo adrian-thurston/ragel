05508d109059e400e093456ce50a1edfc6299f76
Adrian Thurston <thurston@colm.net>
2019-09-09 09:28:18 -0600

    executed slice-testing to remove private dirnames
    Private cases have been pruned with git filter-branch. Removed the sections of
    configure and Makefile.am that ran these cases.

--- autogen.sh
+++ autogen.sh
@@ -8,18 +8,3 @@ autoheader
 automake --foreign --add-missing 
 autoconf
 
-# END PUBLIC
-
-# GENF cases must have autogen.sh called.
-
-cd cases/app1.d
-$HOME/pkgs/pkgbuild/bin/autogen.sh
-cd ../..
-
-cd cases/kern1.d
-$HOME/pkgs/pkgbuild/bin/autogen.sh
-cd ../..
-
-cd cases/process1.d
-$HOME/pkgs/pkgbuild/bin/autogen.sh
-cd ../..
