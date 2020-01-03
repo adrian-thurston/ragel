e673499ac9ca4739bfbf943e527b7624c50504f0
Adrian Thurston <thurston@colm.net>
2020-01-03 11:55:46 +0200

    updated colm-pack wrapper: parse args first then switch on output
    Other updates to colm/Makefile.am: don't depend on gen dir to have it built,
    causes rebuilding when new files go in. Do it the old way using mkdir -p gen in
    the rule that puts files there.
    
    Also colm-pack.sh needs to go into EXTRA_DIST.
    
    refs #85

--- colm/colm-pack.sh
+++ colm/colm-pack.sh
@@ -1,55 +1,56 @@
 #!/bin/bash
 #
 
-if [ $1 = "unpack" ]; then
-	shift 1
-	while getopts "o:" opt; do
-		case $opt in
-			o) parser_output=$OPTARG;;
-		esac
-	done
-	shift $((OPTIND - 1));
+# This wrapper around the colm program (and bootstrap programs) allows us to
+# stick to one output per run of the program. It packs up the multiple colm
+# output files into one file. We can extract them one at a time afterwards.
 
-	tar -xmf "$@" $parser_output
-	exit $?
+CMD=$1;
+shift 1
 
-else
-
-	if [ "`basename $0`" != "$0" ] && [ -x "`dirname $0`/$1" ]; then
-		COLM="`dirname $0`/$1"
-	else
-		COLM=@prefix@/bin/$1
+ARGS=""
+while getopts "p:o:e:x:RcD:I:L:vdlirS:M:vHh?-:sVa:m:b:E:" opt; do
+	# For the unpack case.
+	if [ "$opt" = 'o' ]; then
+		output=$OPTARG
 	fi
 
-	shift 1
+	# For the colm wrapper case.
+	case "$opt" in
+		p)
+			# Pack file name
+			pack_file=$OPTARG
+		;;
+		[oexm]) 
+			ARGS="$ARGS -$opt $OPTARG.pack"
+			PACKS="$PACKS $OPTARG.pack"
+		;;
+		[DILSMambE-])   ARGS="$ARGS -$opt $OPTARG" ;;
+		[RcvdlirvHhsV]) ARGS="$ARGS -$opt" ;;
+		?)
+			exit 1;
+		;;
+	esac
+done
 
-	ARGS=""
+# Shift over the args.
+shift $((OPTIND - 1));
 
-	# Using silent mode to pass other options through.
-	while getopts "p:o:e:x:RcD:I:L:vdlirS:M:vHh?-:sVa:m:b:E:" opt; do
-
-		case $opt in
-			p)
-				# Pack file name
-				pack_file=$OPTARG
-			;;
-			[oexm]) 
-				# Parser Output (C)
-				parser_output=$OPTARG
-				ARGS="$ARGS -$opt $OPTARG.pack"
-				PACKS="$PACKS $OPTARG.pack"
-			;;
-			[DILSMambE-])   ARGS="$ARGS -$opt $OPTARG" ;;
-			[RcvdlirvHhsV]) ARGS="$ARGS -$opt" ;;
-			?)
-				exit 1;
-			;;
-		esac
-	done
-
-	shift $((OPTIND - 1));
+if [ $CMD = "unpack" ]; then
+	tar -xmf "$@" $output
+	EXIT_STATUS=$?
+else
+	if [ "`basename $0`" != "$0" ] && [ -x "`dirname $0`/$CMD" ]; then
+		COLM="`dirname $0`/$CMD"
+	else
+		COLM=@prefix@/bin/$CMD
+	fi
 
 	$COLM $ARGS "$@"
+	EXIT_STATUS=$?
 	tar --transform 's/.pack$//' -cf "$pack_file" $PACKS
 	rm -f $PACKS
 fi
+
+exit $EXIT_STATUS
+
